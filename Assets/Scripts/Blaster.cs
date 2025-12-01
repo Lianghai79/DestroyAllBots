2d0e0f9a5f2b2b1f7b6a01902c83d62dead30410
Lianghai79 <morganlhorrocks@outlook.com>
2025-12-01 15:52:40 +0000

    GunFollow

--- Assets/Scripts/Blaster.cs
+++ Assets/Scripts/Blaster.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Blaster : MonoBehaviour
 {
     public GameObject Player;
-    public Vector2 mousePos;
+    private Vector2 mousePosition;
+    public Vector2 playerPos;
+    public Vector2 direction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +17,15 @@ public class Blaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        playerPos = Player.transform.position;
+        direction += (playerPos - mousePosition);
+        if (direction.magnitude > 0)
+        {
+            direction.Normalize();
+        }
+
+        transform.position = playerPos - (direction*2);
     }
 }
